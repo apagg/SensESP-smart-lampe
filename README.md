28d4ed586abb2c3672588f8f5114d33a4f0ac767
apagg <andreas.pagander@gmail.com>
2023-11-08 19:27:48 +0100

    Bruker nå environment.mode for natt eller dag Ryddet opp i koden

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SensESP Smart Lampe
 
 Lampe som gir hvit sterkt lys på dagen og rødt lys på natten 
+bruker environment.mode for å finne ut om det er natt eller dag
+Kontrolerer WS2812B LED stripe
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
-// Signal K application template file.
+// Signal K application sensESP-smart-lampe.
 //
-// This application demonstrates core SensESP concepts in a very
-// concise manner. You can build and upload the application as is
-// and observe the value changes on the serial port monitor.
-//
-// You can use this source file as a basis for your own projects.
-// Remove the parts that are not relevant to you, and add your own code
-// for external hardware libraries.
+// Lampe som gir hvit sterkt lys på dagen og rødt lys på natten 
+// bruker environment.mode for å finne ut om det er natt eller dag
+// Kontrolerer WS2812B LED stripe
 
 #include "sensesp/sensors/analog_input.h"
 #include "sensesp/sensors/digital_input.h"
@@ -14,23 +10,18 @@
 #include "sensesp/signalk/signalk_output.h"
 #include "sensesp/system/lambda_consumer.h"
 #include "sensesp_app_builder.h"
-// apagg:
 #include "sensesp/signalk/signalk_listener.h"
 #include "sensesp/signalk/signalk_value_listener.h"
-
-// neopixel
 #include <Adafruit_NeoPixel.h>
 
 #define PIN_WS2812B 33  // The ESP32 pin GPIO16 connected to WS2812B
 #define NUM_PIXELS 5    // The number of LEDs (pixels) on WS2812B LED strip
 
 Adafruit_NeoPixel ws2812b(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
-// neopixel
 
 using namespace sensesp;
 
-String nattDag = "sunnset";
-int test = 100;
+String nattDag = "day";
 
 reactesp::ReactESP app;
 
@@ -46,98 +37,34 @@ void setup() {
   SensESPAppBuilder builder;
   sensesp_app = (&builder)
                     // Set a custom hostname for the app.
-                    ->set_hostname("my-sensesp-project")
+                    ->set_hostname("sensESP-smart-lampe")
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
                     //->set_wifi("My WiFi SSID", "my_wifi_password")
                     //->set_sk_server("192.168.10.3", 80)
                     ->get_app();
-
-  /*
-
-
-  // GPIO number to use for the analog input
-  const uint8_t kAnalogInputPin = 36;
-  // Define how often (in milliseconds) new samples are acquired
-  const unsigned int kAnalogInputReadInterval = 500;
-  // Define the produced value at the maximum input voltage (3.3V).
-  // A value of 3.3 gives output equal to the input voltage.
-  const float kAnalogInputScale = 3.3;
-
-
-  // Create a new Analog Input Sensor that reads an analog input pin
-  // periodically.
-
-  auto* analog_input = new AnalogInput(
-      kAnalogInputPin, kAnalogInputReadInterval, "", kAnalogInputScale);
-
-  // Add an observer that prints out the current value of the analog input
-  // every time it changes.
-  analog_input->attach([analog_input]() {
-    debugD("Analog input value: %f", analog_input->get());
-  });
-
-  */
-
-  // Set GPIO pin 15 to output and toggle it every 650 ms
-
-  const uint8_t kDigitalOutputPin = 15;
-  const unsigned int kDigitalOutputInterval = 650;
-  pinMode(kDigitalOutputPin, OUTPUT);
-  app.onRepeat(kDigitalOutputInterval, [kDigitalOutputPin]() {
-    digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
-  });
-
-  // Read GPIO 14 every time it changes
-
-  const uint8_t kDigitalInput1Pin = 14;
-  auto* digital_input1 =
-      new DigitalInputChange(kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
-
-  // Connect the digital input to a lambda consumer that prints out the
-  // value every time it changes.
-
-  // Test this yourself by connecting pin 15 to pin 14 with a jumper wire and
-  // see if the value changes!
-
-  digital_input1->connect_to(new LambdaConsumer<bool>(
-      [](bool input) { debugD("Digital input value changed: %d", input); }));
-
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
+ 
+  // Test om det er beveglese og slå av og på lyset 
   const uint8_t kDigitalInput2Pin = 13;
-  const unsigned int kDigitalInput2Interval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
-  pinMode(kDigitalInput2Pin, INPUT_PULLUP);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
-
+  const unsigned int kDigitalInput2Interval = 250;
+  
+  pinMode(kDigitalInput2Pin, INPUT_PULLDOWN);
+  
   auto* digital_input2 = new RepeatSensor<bool>(
       kDigitalInput2Interval,
       [kDigitalInput2Pin]() { return digitalRead(kDigitalInput2Pin); });
 
   digital_input2->connect_to(new LambdaConsumer<bool>([](bool input) {
-    debugD("********** Digital input value changed: %d", input);
-    debugD("---------- Natt Dag  : %s", nattDag);
-
-    if (input == 1) {  // lyset er av
-      debugD("if : %d", input);
-
+    //debugD("********** Digital input value changed: %d", input);
+    
+    if (input == 0) {  // lyset er av
+      
       for (int pixel = 0; pixel < NUM_PIXELS; pixel++) {  // for each pixel
         ws2812b.setPixelColor(pixel, ws2812b.Color(0, 0, 0));
         ws2812b.show();  // update to the WS2812B Led Strip
       }
     } else {
-      debugD("else : %d", input);
-
+      
       if (nattDag == "night") {
         for (int pixel = 0; pixel < NUM_PIXELS; pixel++) {  // for each pixel
           ws2812b.setPixelColor(pixel, ws2812b.Color(20, 0, 0));
@@ -153,22 +80,7 @@ void setup() {
       }
     }
   }));
-
-  /*
-
-  // Connect the analog input to Signal K output. This will publish the
-  // analog input value to the Signal K server every time it changes.
-  analog_input->connect_to(new SKOutputFloat(
-      "sensors.analog_input.voltage",         // Signal K path
-      "/sensors/analog_input/voltage",        // configuration path, used in the
-                                              // web UI and for storing the
-                                              // configuration
-      new SKMetadata("V",                     // Define output units
-                     "Analog input voltage")  // Value description
-      ));
-
-  */
-
+  
   // Connect digital input 2 to Signal K output.
   digital_input2->connect_to(new SKOutputBool(
       "sensors.digital_input2.value",          // Signal K path
@@ -177,14 +89,14 @@ void setup() {
                      "Digital input 2 value")  // Value description
       ));
 
-  const char* sk_gpsfix = "environment.sun";
+  // Listen for enviroment.mode
+  const char* sk_sun = "environment.mode";
   const int listen_delay = 1000;
-  auto* gpsfix = new StringSKListener(sk_gpsfix, listen_delay);
+  auto* env_sun = new StringSKListener(sk_sun, listen_delay);
 
-  gpsfix->connect_to(new LambdaConsumer<String>([](String fix) {
-    nattDag = fix;
-    test = 200;
-    Serial.printf(" Natt eller dag: %s   ", fix);
+  env_sun->connect_to(new LambdaConsumer<String>([](String sun) {
+    nattDag = sun;
+    Serial.printf(" Natt eller dag: %s   ", sun);
     Serial.println();
   }));
 
