72f33e67d3580639e558446f860e39b1448e1fe9
apagg <andreas.pagander@gmail.com>
2024-01-20 18:55:09 +0100

    Siste versjon per nå

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,20 @@
 #include "DHT.h"
 
 #define PIN_WS2812B 33  // The ESP32 pin GPIO16 connected to WS2812B
-#define NUM_PIXELS 16    // The number of LEDs (pixels) on WS2812B LED strip
+#define NUM_PIXELS 24    // The number of LEDs (pixels) on WS2812B LED strip
 #define DHTPIN 5   // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define PIRPIN 13 // PIR PIN
+
 
 Adafruit_NeoPixel ws2812b(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
 DHT dht(DHTPIN, DHTTYPE);
 
 using namespace sensesp;
 
-String lampeNavn = "test-1";
-String lampeNavnFult = "sensESP-smart-lampe-" + lampeNavn;
-String nattDag = "day";
+String lampe_navn = "salong";
+String lampe_navn_fult = "sensESP-smart-lampe-" + lampe_navn;
+String natt_dag = "day";
 
 reactesp::ReactESP app;
 
@@ -49,7 +51,7 @@ void setup() {
   SensESPAppBuilder builder;
   sensesp_app = (&builder)
                     // Set a custom hostname for the app.
-                    ->set_hostname(lampeNavnFult)
+                    ->set_hostname(lampe_navn_fult)
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
                     //->set_wifi("My WiFi SSID", "my_wifi_password")
@@ -71,25 +73,25 @@ void setup() {
       new RepeatSensor<float>(read_DHT_interval, read_DHT_hum_callback);
 
   // Set the Signal K Path for the output
-  String sk_DHT_temp_path = "enviroment.lampe." + lampeNavn + ".temperature";
-  String sk_DHT_hum_path = "enviroment.lampe." + lampeNavn + ".relativeHumidity";
+  String sk_DHT_temp_path = "environment.lampe." + lampe_navn + ".temperature";
+  String sk_DHT_hum_path = "environment.lampe." + lampe_navn + ".relativeHumidity";
 
   // Send the temperature to the Signal K server as a Float
   lampe_DHT_temp->connect_to(new SKOutputFloat(sk_DHT_temp_path));
   lampe_DHT_hum->connect_to(new SKOutputFloat(sk_DHT_hum_path));
   // Slutt DHT
 
-
+  
   // Test om det er beveglese og slå av og på lyset 
   const uint8_t kPIRInputPin = 13; // Define PIR pin
-  const unsigned int kPIRInputInterval = 250;
-  String sk_PIR_path = "sensors.pir." + lampeNavn + ".value";
-  String sk_PIR_conf_path = "/sensors/pir/" + lampeNavn + "value";
+  unsigned int pir_input_interval = 250;
+  String sk_PIR_path = "sensors.pir." + lampe_navn + ".value";
+  String sk_PIR_conf_path = "/sensors/pir/" + lampe_navn + "/value";
   
   pinMode(kPIRInputPin, INPUT_PULLDOWN);
   
   auto* pir_input = new RepeatSensor<bool>(
-      kPIRInputInterval,
+      pir_input_interval,
       [kPIRInputPin]() { return digitalRead(kPIRInputPin); });
 
   pir_input->connect_to(new LambdaConsumer<bool>([](bool input) {
@@ -103,7 +105,7 @@ void setup() {
       }
     } else {
       
-      if (nattDag == "night") {
+      if (natt_dag == "night") {
         for (int pixel = 0; pixel < NUM_PIXELS; pixel++) {  // for each pixel
           ws2812b.setPixelColor(pixel, ws2812b.Color(50, 0, 0));
           ws2812b.show();  // update to the WS2812B Led Strip
@@ -119,7 +121,7 @@ void setup() {
     }
   }));
   
-  // Connect PIR sensor to Signal K output.
+  // Connect PIR to Signal K output.
   pir_input->connect_to(new SKOutputBool(
       sk_PIR_path,          // Signal K path
       sk_PIR_conf_path,         // configuration path
@@ -133,7 +135,7 @@ void setup() {
   auto* env_sun = new StringSKListener(sk_sun, listen_delay);
   
   env_sun->connect_to(new LambdaConsumer<String>([](String sun) {
-    nattDag = sun;
+    natt_dag = sun;
     Serial.printf(" Natt eller dag: %s   ", sun);
     Serial.println();
   }));
